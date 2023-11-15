d49d33e91ae2428408b715d27fb43254bd03d69f
bogdan <tvorbogdan@gmail.com>
2023-11-15 22:40:53 +0200

    KS-2-1 Созданы переменные с баллами учеников, посчитан и выведен в консоль средний балл в классе

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,11 @@
+package lesson_2
+
+fun main() {
+    val grade1: Float = 3f
+    val grade2: Float = 4f
+    val grade3: Float = 3f
+    val grade4: Float = 5f
+
+    val averageGrade: Float = (grade1+grade2+grade3+grade4)/4
+    println("Средний балл - $averageGrade")
+}
