f333c10a89fc2f28720e83277980091b38a91a98
bogdan <tvorbogdan@gmail.com>
2023-11-16 19:22:34 +0200

    KS-2-1 Решение переделано с использованием списков и функции average()

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,11 +1,7 @@
 package lesson_2
 
 fun main() {
-    val grade1: Float = 3f
-    val grade2: Float = 4f
-    val grade3: Float = 3f
-    val grade4: Float = 5f
-
-    val averageGrade: Float = (grade1+grade2+grade3+grade4)/4
-    println("Средний балл - $averageGrade")
+    val grade = listOf(3, 4, 3, 5)
+    val averageScore = grade.average()
+    println("Средний балл - $averageScore")
 }
