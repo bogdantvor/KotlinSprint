54c359f86b168b5f800beda2ac418e1b175989c6
bogdan <tvorbogdan@gmail.com>
2023-11-16 18:52:40 +0200

    KS-1-3 Время взлёта и посадки исправлено с числового на строчное

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -2,15 +2,15 @@ package lesson_1
 
 fun main() {
     val year = 1961
-    var hour = 9
-    var minute = 7
+    var hour = "09"
+    var minute = "07"
 
     println("Год полёта в космос первого человека - $year")
     println("Час взлёта - $hour")
     println("Минута взлёта - $minute")
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
     println("Время посадки - $hour:$minute")
 }
