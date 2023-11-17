65d7b74667a6944df79cb6ef71d9725b6701c988
bogdan <tvorbogdan@gmail.com>
2023-11-17 22:25:10 +0200

    KS-2-4 Исправлена логика подсчёта бонусов (проценты прописаны явно)

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -3,10 +3,10 @@ package lesson_2
 fun main() {
     val crystalOreEarn = 7
     val ironOreEarn = 11
-    val buffPercentage = 0.2
+    val buffPercentage = 20
 
-    val crystalOreBonusEarn = (crystalOreEarn * (1 + buffPercentage) - crystalOreEarn).toInt()
-    val ironOreBonusEarn = (ironOreEarn * (1 + buffPercentage) - ironOreEarn).toInt()
+    val crystalOreBonusEarn = ((crystalOreEarn * buffPercentage) / 100).toInt()
+    val ironOreBonusEarn = ((ironOreEarn * buffPercentage) / 100).toInt()
 
     println("Количество \"бонусной\" кристаллической руды: $crystalOreBonusEarn")
     println("Количество \"бонусной\" железной руды: $ironOreBonusEarn")
