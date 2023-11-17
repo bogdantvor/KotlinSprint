e9ac0da57ec69d4d2ffd86b088f7b7360bb79eeb
bogdan <tvorbogdan@gmail.com>
2023-11-17 22:41:11 +0200

    KS-2-4 Исправлена логика подсчёта бонусов (проценты прописаны явно). При делении на 100 процентов использован тип Double, чтобы Int не округлял результат деления в 0

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -5,8 +5,8 @@ fun main() {
     val ironOreEarn = 11
     val buffPercentage = 20
 
-    val crystalOreBonusEarn = ((crystalOreEarn * buffPercentage) / 100).toInt()
-    val ironOreBonusEarn = ((ironOreEarn * buffPercentage) / 100).toInt()
+    val crystalOreBonusEarn = (crystalOreEarn * (buffPercentage / 100.0)).toInt()
+    val ironOreBonusEarn = (ironOreEarn * (buffPercentage / 100.0)).toInt()
 
     println("Количество \"бонусной\" кристаллической руды: $crystalOreBonusEarn")
     println("Количество \"бонусной\" железной руды: $ironOreBonusEarn")
