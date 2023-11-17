3d95f54a2ddcbdba30f7feed5cd2e70ebf7fa806
bogdan <tvorbogdan@gmail.com>
2023-11-17 22:13:34 +0200

    KS-1-5 Исправлены имена констант

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -4,13 +4,13 @@ fun main() {
 
     val totalSeconds = 6488
 
-    val hours = totalSeconds / secInHour
-    val minutes = (totalSeconds % secInHour) / secInMinute
-    val seconds = totalSeconds % secInMinute
+    val hours = totalSeconds / SEC_IN_HOUR
+    val minutes = (totalSeconds % SEC_IN_HOUR) / SEC_IN_MINUTE
+    val seconds = totalSeconds % SEC_IN_MINUTE
 
     val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds)
     println("Время проведенное в космосе: $formattedTime")
 }
 
-    const val secInHour = 3600
-    const val secInMinute = 60
+    const val SEC_IN_HOUR = 3600
+    const val SEC_IN_MINUTE = 60
