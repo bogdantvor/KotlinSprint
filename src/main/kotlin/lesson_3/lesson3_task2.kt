5dd4049d4188a70c2e80cd67e1393b2168d683ad
bogdan <tvorbogdan@gmail.com>
2023-11-18 17:45:34 +0200

    KS-3-2 Объявлены и выведены в консоль данные гражданина. Далее данные гражданина изменены и также выведены в консоль

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,13 @@
+package lesson_3
+
+fun main() {
+    val name = "Татьяна"
+    val fathersName = "Сергеевна"
+    var surname = "Андреева"
+    var age = 20
+    println("Данные гражданина: $surname $name $fathersName, $age лет")
+
+    surname = "Сидорова"
+    age = 22
+    println("Данные гражданина: $surname $name $fathersName, $age лет")
+}
