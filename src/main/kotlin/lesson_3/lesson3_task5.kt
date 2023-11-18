b97d5d55063d21de09d3f3027ada2cc6684a6955
bogdan <tvorbogdan@gmail.com>
2023-11-18 17:56:15 +0200

    KS-3-5 Объявлена строка с данными шахматного хода. Прописана логика парсинга данных хода в отдельные переменные и вывод этих переменных в консоль

---
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -0,0 +1,12 @@
+package lesson_3
+
+fun main() {
+    val chessMoveString = "D2-D4;0"
+
+    val (fromTo, moveNumber) = chessMoveString.split(";")
+    val (from, to) = fromTo.split("-")
+
+    println("Откуда: $from")
+    println("Куда: $to")
+    println("Номер хода: $moveNumber")
+}
