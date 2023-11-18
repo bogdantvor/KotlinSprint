66b77b2febc29f2675abe4643e254f6c9e53c57a
bogdan <tvorbogdan@gmail.com>
2023-11-18 17:43:59 +0200

    KS-3-1 Объявлено имя пользователя, выведены в консоль разные варианты приветствия пользователя

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,11 @@
+package lesson_3
+
+fun main() {
+    val name = "Богдан"
+
+    var message = "Доброго дня, $name!"
+    println(message)
+
+    message = "Доброй ночи, $name!"
+    println(message)
+}
