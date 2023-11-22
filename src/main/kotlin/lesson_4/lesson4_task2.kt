910e210b6b90bf1335e192fb464a8ec3bdf07bbe
bogdan <tvorbogdan@gmail.com>
2023-11-22 12:04:04 +0200

    KS-4-2 Отформатировал код для более удобного визуального восприятия. Создал константы для значений, с которым сравниваем входные данные

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,15 +1,17 @@
 package lesson_4
 
 fun main() {
-    val minAverageWeight = 35
-    val maxAverageWeight = 100
-    val maxAverageVolume = 100
-
     val cargo1Weight = 20
     val cargo1Volume = 80
     val cargo2Weight = 50
     val cargo2Volume = 100
 
-    println("Груз с весом $cargo1Weight кг и объемом $cargo1Volume л соответствует категории 'Average': ${minAverageWeight < cargo1Weight && cargo1Weight < maxAverageWeight && cargo1Volume < maxAverageVolume}")
-    println("Груз с весом $cargo2Weight кг и объемом $cargo2Volume л соответствует категории 'Average': ${minAverageWeight < cargo2Weight && cargo2Weight < maxAverageWeight && cargo2Volume < maxAverageVolume}")
+    println("Груз с весом $cargo1Weight кг и объемом $cargo1Volume л соответствует категории 'Average': " +
+            "${MIN_AVERAGE_WEIGHT < cargo1Weight && cargo1Weight < MAX_AVERAGE_WEIGHT && cargo1Volume < MAX_AVERAGE_VOLUME}")
+    println("Груз с весом $cargo2Weight кг и объемом $cargo2Volume л соответствует категории 'Average': " +
+            "${MIN_AVERAGE_WEIGHT < cargo2Weight && cargo2Weight < MAX_AVERAGE_WEIGHT && cargo2Volume < MAX_AVERAGE_VOLUME}")
 }
+
+    const val MIN_AVERAGE_WEIGHT = 35
+    const val MAX_AVERAGE_WEIGHT = 100
+    const val MAX_AVERAGE_VOLUME = 100
