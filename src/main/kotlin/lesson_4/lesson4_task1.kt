cf5e6830a8d14d41fe0e77dac0cb7f31da16dc6a
bogdan <tvorbogdan@gmail.com>
2023-11-22 11:41:37 +0200

    KS-4-1 Убрал "магические числа"

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,10 +1,11 @@
 package lesson_4
 
 fun main() {
-    val totalTables = 13
-    val reservedTablesToday = totalTables
-    val reservedTablesTomorrow = totalTables - 4
+    val freeTablesToday = 0
+    val freeTablesTomorrow = 4
 
-    println("Доступность столиков на сегодня: ${totalTables > reservedTablesToday}\n" +
-            "Доступность столиков на завтра: ${totalTables > reservedTablesTomorrow}")
+    println("Доступность столиков на сегодня: ${TOTAL_TABLES > TOTAL_TABLES - freeTablesToday}\n" +
+            "Доступность столиков на завтра: ${TOTAL_TABLES > TOTAL_TABLES - freeTablesTomorrow}")
 }
+
+    const val TOTAL_TABLES = 13
