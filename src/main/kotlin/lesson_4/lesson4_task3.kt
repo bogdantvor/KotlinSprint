8a82456e1f12a181ce3f0fa0bb6d85f6bc637c25
bogdan <tvorbogdan@gmail.com>
2023-11-22 14:01:15 +0200

    KS-4-3 Исправил названия переменных, добавил константы

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,11 +1,13 @@
 package lesson_4
 
 fun main() {
-    val sunnyWeather: Boolean = true
-    val tentOpen: Boolean = true
-    val airHumidity: Int = 20
+    val isSunny: Boolean = true
+    val isAwningOpen: Boolean = true
+    val currentAirHumidity: Int = 20
     val currentSeason: String = "зима"
 
-    val conditionsSuitable = sunnyWeather && tentOpen && airHumidity == 20 && currentSeason != "зима"
+    val conditionsSuitable = isSunny && isAwningOpen && currentAirHumidity == AIR_HUMIDITY && currentSeason != "зима"
     println("Благоприятные ли условия сейчас для роста бобовых? $conditionsSuitable")
 }
+
+    const val AIR_HUMIDITY = 20
