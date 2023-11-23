dadc5c43522eb6d84208e735a0f0493932dc6075
bogdan <tvorbogdan@gmail.com>
2023-11-23 14:59:07 +0200

    KS-4-3 Добавил ещё одну константу

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -6,8 +6,10 @@ fun main() {
     val currentAirHumidity: Int = 20
     val currentSeason: String = "зима"
 
-    val conditionsSuitable = isSunny && isAwningOpen && currentAirHumidity == AIR_HUMIDITY && currentSeason != "зима"
+    val conditionsSuitable = isSunny && isAwningOpen &&
+            currentAirHumidity == AIR_HUMIDITY && currentSeason != BAD_TIME_OF_YEAR
     println("Благоприятные ли условия сейчас для роста бобовых? $conditionsSuitable")
 }
 
     const val AIR_HUMIDITY = 20
+    const val BAD_TIME_OF_YEAR = "зима"
