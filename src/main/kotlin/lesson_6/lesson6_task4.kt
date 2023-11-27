62e5d8778f903b47bc2af0cea5e41b08969f2823
bogdan <tvorbogdan@gmail.com>
2023-11-27 19:24:53 +0200

    KS-6-4 Программа генерирует случайное число. Пользователю дается 5 попыток, чтобы угадать это число. Если игрок угадывает число, выводится сообщение с поздравлениями и программа завершает работу. Если не угадывает, то отображается сообщение о неудаче и оставшееся количество попыток. После истечения попыток выводится сообщение с загаданным числом

---
+++ src/main/kotlin/lesson_6/lesson6_task4.kt
@@ -0,0 +1,24 @@
+package lesson_6
+
+fun main() {
+    val secretNumber = (1..9).random()
+    var attempts = 5
+
+    println("Попробуйте угадать число от 1 до 9.")
+
+    while (attempts > 0) {
+        print("Введите ваш вариант: ")
+        val guess = readln().toInt()
+
+        if (guess == secretNumber) {
+            println("Это была великолепная игра! Вы угадали число $secretNumber")
+            return
+        } else {
+            println("Неверно. Осталось попыток: ${attempts - 1}")
+        }
+
+        attempts--
+    }
+
+    println("Было загадано число $secretNumber. К сожалению, вы использовали все попытки.")
+}
