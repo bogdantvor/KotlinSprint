f6b1166615d7336098f3545c292549025ddb6f2c
bogdan <tvorbogdan@gmail.com>
2023-11-30 19:53:04 +0200

    KS-7-1 Вынес диапазоны в переменные

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -2,9 +2,12 @@ package lesson_7
 
 fun main() {
     val passwordLength = 6
+    val numberChar = ('0'..'9')
+    val letterChar = ('a'..'z')
+
     val password = buildString {
         for (i in 1..passwordLength) {
-            val char = if (i % 2 == 0) ('0'..'9').random() else ('a'..'z').random()
+            val char = if (i % 2 == 0) numberChar.random() else letterChar.random()
             append(char)
         }
     }
