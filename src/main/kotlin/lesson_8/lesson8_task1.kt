b7de30687e0ecaa4cdde7b2dce91e9a8107edcfd
bogdan <tvorbogdan@gmail.com>
2023-12-03 18:19:35 +0200

    KS-8-1 Массив содержит количество просмотров рекламы за каждый день недели. Далее программа подсчитывает общее количество просмотров рекламы за неделю и выводит это в консоль

---
+++ src/main/kotlin/lesson_8/lesson8_task1.kt
@@ -0,0 +1,9 @@
+package lesson_8
+
+fun main() {
+    val viewsPerDay = arrayOf(247, 415, 381, 512, 624, 486, 594)
+
+    val totalViews = viewsPerDay.sum()
+
+    println("Общее количество просмотров за неделю: $totalViews")
+}
