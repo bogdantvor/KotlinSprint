542873ccd1a153b8320896e1ce09d8e4db34fa9d
bogdan <tvorbogdan@gmail.com>
2023-12-03 18:36:15 +0200

    KS-8-5 Программа запрашивает количество ингредиентов и создаёт массив с нужным количеством элементов. Далее пользователь вводит ингредиенты по одному и они записываются в массив. После, элементы массива выводятся в консоль

---
+++ src/main/kotlin/lesson_8/lesson8_task5.kt
@@ -0,0 +1,18 @@
+package lesson_8
+
+fun main() {
+    print("Введите количество ингредиентов: ")
+    val ingredientCount = readln().toInt()
+
+    val ingredients = Array(ingredientCount) {""}
+
+    for (i in 0 until ingredientCount) {
+        print("Введите ингредиент ${i + 1}: ")
+        ingredients[i] = readln()
+    }
+
+    println("Вы ввели следующие ингредиенты:")
+    for (ingredient in ingredients) {
+        println(ingredient)
+    }
+}
