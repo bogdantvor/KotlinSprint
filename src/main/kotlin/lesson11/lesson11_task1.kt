8fba7d9c0dda6be27fb25b65446c5be741f684d5
bogdan <tvorbogdan@gmail.com>
2023-12-09 19:34:11 +0200

    KS-11-1 Создан класс, который будет хранить данные пользователя, и два экземпляра класса с произвольными данными. Содержимое полей экземпляров выведено в консоль

---
+++ src/main/kotlin/lesson11/lesson11_task1.kt
@@ -0,0 +1,22 @@
+package lesson11
+
+class User(
+    val userId: String,
+    val login: String,
+    val password: String,
+    val email: String
+)
+
+fun main() {
+    val user1 = User("1", "Alex", "Smirn123", "alexsmirn@example.com")
+    val user2 = User("2", "Ivan", "ivanqwerty", "ivanovivan@example.com")
+
+    printUserInfo(user1)
+    printUserInfo(user2)
+}
+
+fun printUserInfo(user: User): String {
+    val userInfo = "Данные юзера: userId=${user.userId}, login=${user.login}, password=${user.password}, email=${user.email}"
+    println(userInfo)
+    return userInfo
+}
