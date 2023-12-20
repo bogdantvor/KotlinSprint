bbc15fc2afd02149d2982e9b185ae1919b58caff
bogdan <tvorbogdan@gmail.com>
2023-12-21 00:06:56 +0200

    KS-14-3 Цвета вынесены в константы

--- src/main/kotlin/lesson14/lesson14_task3.kt
+++ src/main/kotlin/lesson14/lesson14_task3.kt
@@ -20,22 +20,25 @@ class Rectangle(color: String, val width: Double, val height: Double) : Figure(c
 }
 
 fun main() {
-    val blackCircle = Circle(color = "Black", radius = 5.0)
-    val whiteCircle = Circle(color = "White", radius = 7.0)
+    val blackCircle = Circle(color = BLACK_COLOR, radius = 5.0)
+    val whiteCircle = Circle(color = WHITE_COLOR, radius = 7.0)
 
-    val blackRectangle = Rectangle(color = "Black", width = 4.0, height = 6.0)
-    val whiteRectangle = Rectangle(color = "White", width = 8.0, height = 3.0)
+    val blackRectangle = Rectangle(color = BLACK_COLOR, width = 4.0, height = 6.0)
+    val whiteRectangle = Rectangle(color = WHITE_COLOR, width = 8.0, height = 3.0)
 
     val figures = listOf(blackCircle, whiteCircle, blackRectangle, whiteRectangle)
 
     val sumOfBlackPerimeters = figures
-        .filter { it.color.equals("Black", ignoreCase = true) }
+        .filter { it.color.equals(BLACK_COLOR, ignoreCase = true) }
         .sumOf { it.calculatePerimeter() }
 
     val sumOfWhiteAreas = figures
-        .filter { it.color.equals("White", ignoreCase = true) }
+        .filter { it.color.equals(WHITE_COLOR, ignoreCase = true) }
         .sumOf { it.calculateArea() }
 
     println("Сумма периметров всех черных фигур: $sumOfBlackPerimeters")
     println("Сумма площадей всех белых фигур: $sumOfWhiteAreas")
 }
+
+const val BLACK_COLOR = "Black"
+const val WHITE_COLOR = "White"
