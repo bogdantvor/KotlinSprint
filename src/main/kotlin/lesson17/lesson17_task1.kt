3dc3dacba629044d9747c48855f486d6b1c20b85
bogdan <tvorbogdan@gmail.com>
2024-01-09 19:52:10 +0200

    KS-17-1 Создан класс, описывающий элемент викторины. В primary конструкторе два текстовых свойства: вопрос и ответ. Явно прописан для вопроса геттер, для ответа геттер и сеттер

---
+++ src/main/kotlin/lesson17/lesson17_task1.kt
@@ -0,0 +1,26 @@
+package lesson17
+
+class QuizElement(private var _question: String, private var _answer: String) {
+    val question: String
+        get() = _question
+
+    val answer: String
+        get() = _answer
+
+    var customAnswer: String
+        get() = _answer
+        set(value) {
+            _answer = value
+        }
+}
+
+fun main() {
+    val quizElement = QuizElement("Как называется единица измерения времени, равная 60 секундам?", "Минута")
+
+    println("Вопрос: ${quizElement.question}")
+    println("Ответ: ${quizElement.answer}")
+
+    quizElement.customAnswer = "Час"
+
+    println("Измененный ответ: ${quizElement.customAnswer}")
+}
