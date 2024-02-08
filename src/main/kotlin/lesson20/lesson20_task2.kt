4df23a5e50c4c87f3b001be26f045a6e60fec27d
bogdan <tvorbogdan@gmail.com>
2024-02-08 19:11:11 +0200

    KS-20-2 Исправлено название класса

--- src/main/kotlin/lesson20/lesson20_task2.kt
+++ src/main/kotlin/lesson20/lesson20_task2.kt
@@ -1,13 +1,13 @@
 package lesson20
 
-class Player(val name: String, var currentHealth: Int, val maxHealth: Int)
+class Player20_2(val name: String, var currentHealth: Int, val maxHealth: Int)
 
-val healPotion: (Player) -> Unit = { player ->
+val healPotion: (Player20_2) -> Unit = { player ->
     player.currentHealth = player.maxHealth
 }
 
 fun main() {
-    val player = Player("bogdanTV", 50, 100)
+    val player = Player20_2("bogdanTV", 50, 100)
 
     println("Перед использованием зелья: Здоровье игрока ${player.name}: ${player.currentHealth}/${player.maxHealth}")
 
